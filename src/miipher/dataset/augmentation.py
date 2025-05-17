59b8e7a71314925e8e376f60700a179dfe5160e4
kimmchii <thanachotkim@hotmail.co.th>
2025-05-17 19:53:25 +0700

    chore: removed redundant func

--- src/miipher/dataset/augmentation.py
+++ src/miipher/dataset/augmentation.py
@@ -10,22 +10,6 @@ from pathlib import Path
 from tqdm import tqdm
 
 
-def align_waveform(wav1, wav2):
-    assert wav2.size(1) >= wav1.size(1)
-    diff = wav2.size(1) - wav1.size(1)
-    min_mse = float("inf")
-    best_i = -1
-
-    for i in range(diff):
-        segment = wav2[:, i : i + wav1.size(1)]
-        mse = torch.mean((wav1 - segment) ** 2).item()
-        if mse < min_mse:
-            min_mse = mse
-            best_i = i
-
-    return best_i, wav2[:, best_i : best_i + wav1.size(1)]
-
-
 class AudioAugmentationApplier:
     def __init__(self, cfg) -> None:
         self.format_encoding_pairs = cfg.format_encoding_pairs
